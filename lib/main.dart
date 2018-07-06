07bdaa24df0464defb3c54a899462e14c5df556b
julio <jmcarazas@gmail.com>
2018-07-06 03:42:14 -0500

    usando cards stateless y stateful

--- lib/main.dart
+++ lib/main.dart
@@ -10,18 +10,20 @@ class MyApp extends StatelessWidget {
 }
 
 class MyCard extends StatelessWidget {
-  List cards = new List.generate(20, (i) => new CustomCard());
+  List cards = new List.generate(10, (i) => new CustomCard()).toList();
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Cards en Flutter'),
-          backgroundColor: new Color(0xFF673AB7),
-        ),
-        body: new Container(
-            child: new ListView(
+      appBar: new AppBar(
+        title: new Text('Cards en Flutter'),
+        backgroundColor: new Color(0xFF673AB7),
+      ),
+      body: new Container(
+        child: new ListView(
           children: cards,
-        )));
+        )
+      )
+    );
   }
 }
 
@@ -31,34 +33,43 @@ class CustomCard extends StatelessWidget {
     return new Card(
       child: new Column(
         children: <Widget>[
-          new Image.network(
-              'https://flutter.io/images/flutter-logo-sharing.png'),
+          new Image.network('https://cde.peru.com//ima/0/0/6/9/2/692555/611x458/lima.jpg'),
+          new Container(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              '''Wait a minute. Wait a minute, Doc. Uhhh...Are you telling me that you built a time machine...out of a DeLorean?! Whoa. This is heavy..''',
+              softWrap: true,
+            ),
+          ),
           new Padding(
-              padding: new EdgeInsets.all(7.0),
-              child: new Row(
-                children: <Widget>[
-                  new Padding(
-                    padding: new EdgeInsets.all(7.0),
-                    child: new Icon(Icons.thumb_up),
-                  ),
-                  new Padding(
-                    padding: new EdgeInsets.all(7.0),
-                    child: new Text(
-                      'Like',
-                      style: new TextStyle(fontSize: 18.0),
-                    ),
-                  ),
-                  new Padding(
-                    padding: new EdgeInsets.all(7.0),
-                    child: new Icon(Icons.comment),
-                  ),
-                  new Padding(
-                    padding: new EdgeInsets.all(7.0),
-                    child: new Text('Comments',
-                        style: new TextStyle(fontSize: 18.0)),
-                  )
-                ],
-              ))
+            padding: new EdgeInsets.all(7.0),
+            child: new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                new FlatButton.icon(
+                  icon: const Icon(Icons.favorite, size: 18.0, color: Colors.redAccent),
+                  label: const Text('Like'),
+                  onPressed: () {
+                    print('Me encanta');
+                  },
+                ),
+                new FlatButton.icon(
+                  icon: const Icon(Icons.comment, size: 18.0, color: Colors.brown),
+                  label: const Text('Comment'),
+                  onPressed: () {
+                    print('Comenta algo');
+                  },
+                ),
+                new FlatButton.icon(
+                  icon: const Icon(Icons.share, size: 18.0, color: Colors.blueAccent),
+                  label: const Text('Share'),
+                  onPressed: () {
+                    print('Compartelo');
+                  },
+                )
+              ],
+            )
+          )
         ],
       ),
     );
